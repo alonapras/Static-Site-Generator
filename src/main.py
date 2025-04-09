458051b889f0e86734e3807014f4c54b8dd5edf0
Alona Praslov <alona@praslov.com>
2025-04-09 22:21:35 +0300

    Changed public directory to docs. Defined content/stratic/template paths relative to the python app

--- src/main.py
+++ src/main.py
@@ -5,14 +5,19 @@ import sys
 from copystatic import copy_files_recursive
 from gencontent import generate_pages_recursive
 
+
+root_dir = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), ".."
+)
+
 # use the sys.argv to grab the first CLI argument to the program. Save it as the basepath. If one isn't provided, default to /.
 basepath = sys.argv[1] if len(sys.argv) > 1 else "/"
 
 # wsl
-dir_path_static = "/mnt/d/workspace/notebook/Boot.Dev/08_Build-Static-Site-Generator/static"   
-dir_path_public = "/mnt/d/workspace/notebook/Boot.Dev/08_Build-Static-Site-Generator/public"   
-dir_path_content = "/mnt/d/workspace/notebook/Boot.Dev/08_Build-Static-Site-Generator/content"
-template_path = "/mnt/d/workspace/notebook/Boot.Dev/08_Build-Static-Site-Generator/template.html"   
+dir_path_static = os.path.join(root_dir, "static")
+dir_path_public = os.path.join(root_dir, "docs")
+dir_path_content = os.path.join(root_dir, "content")
+template_path = os.path.join(root_dir, "template.html")
 
 def main():
     print("Deleting public directory...")
